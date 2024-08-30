69e22ed9fb0eb37f1ddc43ad4a60be83b2180c79
yayugu <yayugu@gmail.com>
2024-08-30 14:46:49 +0900

    Initial Commit

---
+++ DroggerSampleAppiOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DroggerSampleAppiOS
+//
+//  Created by yayugu on 2024/08/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DroggerSampleAppiOS/DroggerSampleAppiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  DroggerSampleAppiOSApp.swift
+//  DroggerSampleAppiOS
+//
+//  Created by yayugu on 2024/08/30.
+//
+
+import SwiftUI
+
+@main
+struct DroggerSampleAppiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
